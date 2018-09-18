d150d69b606c74f9ddc3a2260c8bac103f6bd1c9
Bruce Makaaru <brucemakallan@gmail.com>
2018-09-18 17:35:57 +0300

    Initialize Components

---
+++ src/components/Game.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Game extends Component {
+  render() {
+    return <div className="Game container-fluid">Game here</div>;
+  }
+}
+
+export default Game;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header text-center border-bottom">
+        <h1>Tick Tack Toe</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
